7baa82d7383a3dbe2324624acda3320bf64fa661
Ramnarayan <ramnarayansah180@gmail.com>
2025-04-26 07:55:03 +0545

    only staic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import Data from "./component/Data";
+
 
 export default function Page(): React.ReactElement {
   return (
     <>
       <div>
         hello world
-        <Data />
+      
       </div>
     </>
   );
