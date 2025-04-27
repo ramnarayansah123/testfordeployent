cb856e815ed479c8c0117ce456320abd99c633ad
Ramnarayan <ramnarayansah180@gmail.com>
2025-04-27 16:26:27 +0545

    fixed title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
+export const metadata: Metadata = {
+  title: "FreeFire Topup",
+  description: "FreeFire Topup is best for nepal users ,freefire topup center is for affordable ,trusty and fast",
+  keywords: "Freefire Topup, Freefire topup center, PUBG topup,  freefire diamond topup"
+  
+};
 
 export default function RootLayout({
   children,
